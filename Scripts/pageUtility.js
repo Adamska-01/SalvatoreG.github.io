d8c476cf500bb355c98eafbcacf66a4bd1eda47b
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-02-24 23:16:48 +0000

    Added image zoom feature

--- Scripts/pageUtility.js
+++ Scripts/pageUtility.js
@@ -26,3 +26,26 @@ function stopYoutubeVideos()
 		this.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');
 	});
 }
+
+async function zoomImageIn(imageSource) 
+{
+    var src = imageSource.src;
+
+    var img = new Image();
+    img.src = src;
+
+    await new Promise((resolve) => 
+	{
+        img.onload = function() 
+		{
+            $(`#${IMAGE_ZOOM_OVERLAY_ID}`).attr("src", src);
+            $(`#${IMAGE_ZOOM_CONTAINER_ID}`).attr('style', 'display: flex;');
+            $(`#${IMAGE_ZOOM_OVERLAY_ID}`).attr('style', 'animation: fade-in var(--change-tabPage-time) ease forwards;');
+        };
+    });
+}
+
+function hideZoomImage()
+{
+	$(`#${IMAGE_ZOOM_CONTAINER_ID}`).css('display', 'none');
+}
