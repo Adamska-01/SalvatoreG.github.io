047c61b0a26191e13760c33027dc5d5cba89715d
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-04-07 20:23:03 +0100

    Added HDRI light to the scene

--- Scripts/3D_Scene/sceneIndex.js
+++ Scripts/3D_Scene/sceneIndex.js
@@ -2,11 +2,13 @@ import * as THREE from 'three';
 import { SceneManager } from './ThreeSubSystems/sceneManager.js';
 import { RenderManager } from './ThreeSubSystems/renderManager.js';
 import { CameraManager } from './ThreeSubSystems/cameraManager.js';
+import { HDRILoader } from './Loaders/HDRILoader.js';
+import { LoadingManager } from './Loaders/loadingManager.js';
 
 
 const container = document.getElementById(THREE_CONTAINER_ID);
 
-// Create THREE.JS sub-systems
+// THREE.JS sub-systems
 const renderManager = new RenderManager(container);
 const cameraManager = new CameraManager(container, renderManager.renderer);
 const sceneManager = new SceneManager();
@@ -16,10 +18,27 @@ const camera = cameraManager.camera;
 
 renderManager.initialise(scene, camera);
 
+// Content Loaders
+const loadingManager = new LoadingManager()
+
+var lightOptions = {
+    mapping: THREE.EquirectangularReflectionMapping,
+    outputEncoding: THREE.sRGBEncoding,
+    toneMapping: true,
+	setHDRAsBackground: true,
+    toneMappingExposure: 2
+};
+const hdriLightLoader = new HDRILoader(scene, renderManager.renderer, loadingManager.manager);
+hdriLightLoader.loadHDRI(new URL(HDRI_PATH), lightOptions);
+
 // Create cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffaf00 } );
-const cube = new THREE.Mesh( geometry, material );
+const cube = new THREE.Mesh( 
+	new THREE.BoxGeometry( 1, 1, 1 ), 
+	new THREE.MeshStandardMaterial({
+		roughness: 1,
+		color: 0xffaf00
+	}) 
+);
 
 sceneManager.scene.add(cube);
 
@@ -30,12 +49,12 @@ function animate()
 {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.005;
-	cube.rotation.y += 0.005;
+	let deltaTime = clock.getDelta();
+	
+	sceneManager.update(deltaTime);
 
-	sceneManager.update(clock.getDelta());
-
-	renderManager.update(clock.getDelta())
+	renderManager.update(deltaTime);
+	cameraManager.update(deltaTime);
 }
 animate();
 
