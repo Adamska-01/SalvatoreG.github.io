3d7e4fdc74571ed032fa90d00ce9685eb49c191b
Adamska-01 <silverworld@hotmail.it>
2025-07-21 00:27:15 +0200

    Implemented model animations

--- Scripts/3D_Scene/Models/codingSetup.js
+++ Scripts/3D_Scene/Models/codingSetup.js
@@ -1,4 +1,5 @@
 import { EntityObject } from "../entityObject.js";
+import * as THREE from 'three';
 
 
 export class CodingSetup extends EntityObject
@@ -12,12 +13,29 @@ export class CodingSetup extends EntityObject
 		loader.loadGLTFModel(modelURL.href, (gltf) => {
 			this.entity = gltf.scene;
 			this.entity.position.set(0.0, -5.0, 0.0);
+			
 			sceneManager.AddObjectToScene(this);
+
+			// Setup animations
+			if (gltf.animations && gltf.animations.length > 0) {
+				this.animationMixer = new THREE.AnimationMixer(this.entity);
+
+				for (let clip of gltf.animations) {
+					const action = this.animationMixer.clipAction(clip);
+					action.loop = THREE.LoopRepeat;
+					action.play();
+				}
+			}
 		});
 	}
 
 	update(deltaTime)
 	{
 		super.update(deltaTime);
+
+		if (this.animationMixer) 
+		{
+			this.animationMixer.update(deltaTime);
+		}
 	}
 }
